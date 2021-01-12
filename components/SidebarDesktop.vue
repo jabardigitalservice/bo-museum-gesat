f76b7d66e6e8b11801b979eacdda3cb8368736f5
Muhammad Adzhar Amrullah <adzharamrullah@gmail.com>
2021-01-12 11:40:01 +0700

    refactor menu mobile and desktop create a component

--- components/SidebarDesktop.vue
+++ components/SidebarDesktop.vue
@@ -1,41 +1,6 @@
 <template>
   <div class="hidden md:w-64 md:block h-screen py-4 bg-primary">
-    <div class="flex justify-center">
-      <h1 class="text-white text-2xl font-medium font-sans">
-        JDS Reservasi
-      </h1>
-    </div>
-    <div class="flex justify-center">
-      <div class="mt-6 w-16 h-16 bg-white rounded-full" />
-    </div>
-    <div class="mt-8">
-      <ul>
-        <li class="pl-4">
-          <NuxtLink to="/dashboard" class="flex items-center py-4 pl-8">
-            <i class="bx bx-home bx-sm text-white" />
-            <span class="text-md font-medium text-white">Dashboard</span>
-          </NuxtLink>
-        </li>
-        <li class="pl-4">
-          <NuxtLink to="/reservasi" class="flex items-center py-4 pl-8">
-            <i class="bx bx-calendar bx-sm text-white" />
-            <span class="text-md font-medium text-white">Reservasi</span>
-          </NuxtLink>
-        </li>
-        <li class="pl-4">
-          <NuxtLink to="/resource" class="flex items-center py-4 pl-8">
-            <i class="bx bx-cabinet bx-sm text-white" />
-            <span class="text-md font-medium text-white">Resource/Aset</span>
-          </NuxtLink>
-        </li>
-        <li class="pl-4">
-          <NuxtLink to="/" class="flex items-center py-4 pl-8">
-            <i class="bx bx-log-out bx-sm text-white" />
-            <span class="text-md font-medium text-white">Logout</span>
-          </NuxtLink>
-        </li>
-      </ul>
-    </div>
+    <Menu />
   </div>
 </template>
 <script>
@@ -43,13 +8,3 @@ export default {
 
 }
 </script>
-<style scoped>
-span {
-  margin-left: 20px;
-}
-a.nuxt-link-exact-active {
-  background: #008444;
-  border-top-left-radius: 8px;
-  border-bottom-left-radius: 8px;
-}
-</style>
--- components/SidebarMobile.vue
+++ components/SidebarMobile.vue
@@ -1,42 +1,7 @@
 <template>
   <transition name="slide">
     <div v-if="isActiveSidebar" v-click-outside="closeSidebar" class="w-64 fixed z-20 h-screen py-4 bg-primary sidebar">
-      <div class="flex justify-center">
-        <h1 class="text-white text-2xl font-medium font-sans">
-          JDS Reservasi
-        </h1>
-      </div>
-      <div class="flex justify-center">
-        <div class="mt-6 w-16 h-16 bg-white rounded-full" />
-      </div>
-      <div class="mt-8">
-        <ul>
-          <li class="pl-4">
-            <NuxtLink to="/dashboard" class="flex items-center py-4 pl-8">
-              <i class="bx bx-home bx-sm text-white" />
-              <span class="text-md font-medium text-white">Dashboard</span>
-            </NuxtLink>
-          </li>
-          <li class="pl-4">
-            <NuxtLink to="/reservasi" class="flex items-center py-4 pl-8">
-              <i class="bx bx-calendar bx-sm text-white" />
-              <span class="text-md font-medium text-white">Reservasi</span>
-            </NuxtLink>
-          </li>
-          <li class="pl-4">
-            <NuxtLink to="/resource" class="flex items-center py-4 pl-8">
-              <i class="bx bx-cabinet bx-sm text-white" />
-              <span class="text-md font-medium text-white">Resource/Aset</span>
-            </NuxtLink>
-          </li>
-          <li class="pl-4">
-            <NuxtLink to="/" class="flex items-center py-4 pl-8">
-              <i class="bx bx-log-out bx-sm text-white" />
-              <span class="text-md font-medium text-white">Logout</span>
-            </NuxtLink>
-          </li>
-        </ul>
-      </div>
+      <Menu />
     </div>
   </transition>
 </template>
@@ -68,14 +33,6 @@ export default {
 }
 .slide-leave-to {
   transform: translate(-100%, 0);
-}
-span {
-  margin-left: 20px;
-}
-a.nuxt-link-exact-active {
-  background: #008444;
-  border-top-left-radius: 8px;
-  border-bottom-left-radius: 8px;
 }
 .sidebar{
   box-shadow: 6px 0px 10px 0px rgba(0,0,0,0.2)
