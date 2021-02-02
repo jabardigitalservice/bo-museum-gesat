8e4cf451942cde0b18b8b2b745aacfe1c25aab20
yoslie <65396086+yoslie@users.noreply.github.com>
2021-02-02 10:06:46 +0700

    moving statistic from dashboard to reservasi

--- pages/index.vue
+++ pages/index.vue
@@ -7,49 +7,10 @@
         </h2>
       </div>
     </div>
-    <div class="md:items-center p-4">
-      <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-        <div
-          v-for="(card, i) in cards"
-          :key="i"
-          :class="['md:col-span-1 text-white p-4 rounded-md', card.bgColor]"
-        >
-          <div class="md:grid md:grid-cols-5 flex item-center">
-            <div class="md:col-span-4 w-full">
-              <div class="text-lg">
-                {{ card.title }}
-              </div>
-              <div class="text-4xl font-bold">
-                {{ card.value }}
-              </div>
-            </div>
-            <div class="md:col-span-1 flex justify-center items-center w-full">
-              <i :class="['bx bx-md text-white', card.icon]" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 <script>
-import { cardsDashboard } from '~/assets/constant/enum'
 export default {
-  layout: 'admin',
-  data () {
-    return {
-      cards: cardsDashboard
-    }
-  },
-  mounted () {
-    this.$axios.get('/dashboard/reservation-statistic').then((res) => {
-      // note: identifier can't use camel case
-      const { all, already_approved: approved, rejected } = res.data || {}
-      this.cards[0].value = all || 0
-      this.cards[1].value = approved || 0
-      this.cards[2].value = rejected || 0
-    })
-  }
+  layout: 'admin'
 }
-
 </script>
