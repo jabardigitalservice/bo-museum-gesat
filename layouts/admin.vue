fe2275ff0e908baeb4ea00bbeed099c26ac7bd8d
Adzhar Amrullah <adzharamrullah@gmail.com>
2021-02-25 09:05:45 +0700

    remove header

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -2,8 +2,7 @@
   <div class="flex w-screen h-screen">
     <SidebarDesktop />
     <SidebarMobile />
-    <div class="w-full overflow-x-auto">
-      <!-- <Header /> -->
+    <div class="w-full overflow-x-auto pt-8">
       <Nuxt />
     </div>
     <div v-if="isActiveSidebar" class="fixed z-10 w-screen h-screen bg-gray1 opacity-50" />
