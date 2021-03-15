f20b5bdd31eda29d5667c3f0f99c7e9c837dd2d9
Bangun Bagustapa <bangunbagustapa@gmail.com>
2021-03-15 10:51:23 +0700

    Add padding for menu and align text to center for user name

--- components/Menu.vue
+++ components/Menu.vue
@@ -9,12 +9,12 @@
       <div class="mt-6 w-16 h-16 bg-white rounded-full" />
     </div>
     <div class="flex justify-center mt-3">
-      <span class="font-medium text-white">{{ this.$auth.user && this.$auth.user.name ? this.$auth.user.name : '' }}</span>
+      <span class="font-medium text-white pr-4 text-center">{{ this.$auth.user && this.$auth.user.name ? this.$auth.user.name : '' }}</span>
     </div>
     <div class="mt-8">
       <ul>
         <li v-for="menu in menus" :key="menu.path" class="pl-4">
-          <NuxtLink :to="menu.path" class="flex items-center py-4 pl-8">
+          <NuxtLink :to="menu.path" class="flex items-center p-4 pr-6">
             <i :class="['bx bx-sm text-white', menu.iconClass]" />
             <span class="text-md font-medium text-white">{{ menu.label }}</span>
           </NuxtLink>
