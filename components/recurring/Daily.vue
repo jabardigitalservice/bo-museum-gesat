688e2fbf3c6eea00ec086f27d85ddf745f19644c
Ida Bagus Wedagama <wardana.wedagama@gmail.com>
2021-05-17 14:23:50 +0700

    refactor Daily.vue: add recurringMixins; remove unused code

--- components/recurring/Daily.vue
+++ components/recurring/Daily.vue
@@ -48,54 +48,13 @@
 </template>
 
 <script>
-import { days } from '../../assets/constant/enum'
+import recurringMixins from './mixins/recurringMixins'
 
 export default {
-  props: {
-    formDays: {
-      type: Array,
-      default: () => ([])
-    },
-    formStartDate: {
-      type: String,
-      default: null
-    },
-    formEndDate: {
-      type: String,
-      default: null
-    }
-  },
+  mixins: [recurringMixins],
   data () {
     return {
-      dropdownOpened: false,
-      days
-    }
-  },
-  computed: {
-    disabledDates () {
-      const yesterday = new Date(new Date().setDate(new Date().getDate() - 1))
-      const startDate = new Date(this.formStartDate)
-      const oneYearAhead = yesterday.setFullYear(new Date().getFullYear() + 1)
-      const tenYearsAhead = yesterday.setFullYear(new Date().getFullYear() + 10)
-      return {
-        to: startDate,
-        ranges: [{
-          from: oneYearAhead,
-          to: tenYearsAhead
-        }]
-      }
-    },
-    selectedDays () {
-      const days = this.formDays
-        .map(formDay => this.days.find(day => day.index === formDay))
-        .sort((a, b) => a.index === 0 ? 1 : b.index === 0 ? -1 : a.index - b.index)
-        .map(formday => formday.name)
-      return days.join(', ')
-    },
-    endDate () {
-      const startDate = this.disabledDates.to
-      const endDate = this.formEndDate
-      return endDate < startDate.toISOString() ? startDate : endDate
+      dropdownOpened: false
     }
   },
   methods: {
