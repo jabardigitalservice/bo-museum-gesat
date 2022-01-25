1389dd10c3efb2e95da051abc913b1a2881f54d6
Doohan <doohanadityas@gmail.com>
2022-01-25 10:23:26 +0700

    feat: Create title component

---
+++ components/Title.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <div class="bg-white flex justify-between w-max-width my-4 p-4 rounded-xl shadow-sm shadow-gray6">
+      <div class="pt-2 self-end md:hidden" @click="openSidebar">
+        <jds-icon class="text-[#000000]" name="menu-burger" size="lg" />
+      </div>
+      <h1 class="text-2xl font-semibold font-sans text-[#000000] self-center md:text-3xl">
+        <slot />
+      </h1>
+      <div class="w-10 h-10 bg-primary rounded-full self-center">
+        <h1 class=" text-white text-2xl leading-relaxed font-bold font-sans text-center">
+          {{ this.$auth.user && this.$auth.user.name ? this.$auth.user.name.charAt(0) : null }}
+        </h1>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  methods: {
+    openSidebar () {
+      this.$store.commit('menu/SET_MENU', true)
+    }
+  }
+}
+</script>
--- layouts/admin.vue
+++ layouts/admin.vue
@@ -3,16 +3,14 @@
     <SidebarDesktop />
     <SidebarMobile />
     <div class="w-full overflow-x-auto">
-      <Header />
       <Nuxt />
     </div>
     <div v-if="isActiveSidebar" class="fixed z-10 w-screen h-screen bg-gray1 opacity-50" />
   </div>
 </template>
 <script>
-import Header from '~/components/Header.vue'
+
 export default {
-  components: { Header },
   computed: {
     isActiveSidebar () {
       return this.$store.state.menu.isActiveSidebar
