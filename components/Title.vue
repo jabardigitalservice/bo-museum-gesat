759b193c618bc88b889e478edd1c244e5e5e00de
Doohan Aditya Saputro <doohanaditya@yahoo.co.id>
2022-01-31 10:38:00 +0700

    refactor(components and layout): rename color in menu, title, admin

--- components/Title.vue
+++ components/Title.vue
@@ -1,13 +1,13 @@
 <template>
   <section>
-    <div class="bg-white flex justify-between w-max-width my-4 p-4 rounded-xl shadow-sm shadow-gray6">
+    <div class="bg-white flex justify-between w-max-width my-4 p-4 rounded-xl shadow-sm shadow-gray-200">
       <div class="pt-2 self-end md:hidden" @click="openSidebar">
         <jds-icon class="text-black" name="menu-burger" size="lg" />
       </div>
-      <h1 class="text-2xl font-semibold font-sans text-[#000000] self-center md:text-3xl">
+      <h1 class="text-2xl font-semibold font-sans text-black self-center md:text-3xl">
         <slot />
       </h1>
-      <div class="w-10 h-10 bg-primary rounded-full text-white text-2xl leading-relaxed font-bold font-sans text-center self-center">
+      <div class="w-10 h-10 bg-green-700 rounded-full text-white text-2xl leading-relaxed font-bold font-sans text-center self-center">
         {{ initialUserName }}
       </div>
     </div>
